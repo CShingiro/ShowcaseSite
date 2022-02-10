fc8083a3b626d9913b7fa1cd535f4ab784ae3820
CShingiro <nemyent@gmail.com>
2022-02-10 17:09:40 -0500

    Attempt patch to php file 2

--- public/index.php
+++ public/index.php
@@ -3,6 +3,10 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+include_once dirname(_DIR_).'/opt/alt/php81/usr/share/pear';
+include_once dirname(_DIR_).'/opt/alt/php81/usr/share/php';
+include_once dirname(_DIR_).'/usr/share/pear';
+include_once dirname(_DIR_).'/usr/share/php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
