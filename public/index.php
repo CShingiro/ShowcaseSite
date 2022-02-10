c30aedbf7f672e1665307915fc993a2607c03425
CShingiro <nemyent@gmail.com>
2022-02-10 17:18:06 -0500

    revert for index.php

--- public/index.php
+++ public/index.php
@@ -3,10 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-include_once dirname(_DIR_).'/opt/alt/php81/usr/share/pear';
-include_once dirname(_DIR_).'/opt/alt/php81/usr/share/php';
-include_once dirname(_DIR_).'/usr/share/pear';
-include_once dirname(_DIR_).'/usr/share/php';
+include_path='.:/opt/alt/php81/usr/share/pear:/opt/alt/php81/usr/share/php:/usr/share/pear:/usr/share/php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
